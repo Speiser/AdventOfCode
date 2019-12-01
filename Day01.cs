e4bbfe89af1df422fa15fa04a077f7b9e372db70
Bernhard Speiser <bernhard.speiser@yahoo.com>
2019-12-01 09:17:01 +0100

    Added Day1

---
+++ Day01.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day01
+    {
+        public static int Puzzle1()
+            => GetPuzzleInput().Sum(CalculateFuel);
+
+        public static int Puzzle2()
+        {
+            var acc = 0;
+            foreach (var input in GetPuzzleInput())
+            {
+                var result = CalculateFuel(input);
+                while (result > 0)
+                {
+                    acc += result;
+                    result = CalculateFuel(result);
+                }
+            }
+            return acc;
+        }
+
+        private static int CalculateFuel(int i) => (i / 3) - 2;
+
+        private static IEnumerable<int> GetPuzzleInput()
+            => File.ReadAllLines("Input/Day01.txt").Select(i => int.Parse(i));
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Day01.Puzzle1()); // 3363760
+            Console.WriteLine(Day01.Puzzle2()); // 5042767
+        }
+    }
+}
