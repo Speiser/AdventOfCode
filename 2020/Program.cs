712492cadace0ceb767ce93e5701faa1172506b4
Bernhard Speiser <bernhard.speiser@yahoo.com>
2020-11-30 13:03:12 +0100

    Add files via upload

--- 2020/Program.cs
+++ 2020/Program.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using AdventOfCode2020.Solutions;
+using AdventOfCode2020.Solutions.Shared;
 
 namespace AdventOfCode2020
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            Solver.Solve<Day01>();
         }
     }
 }
