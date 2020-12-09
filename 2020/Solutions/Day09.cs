0a943f48bf281ca03ca4c0ecfd4ee6b0ecaf5ab4
Bernhard Speiser <bernhard.speiser@yahoo.com>
2020-12-09 13:50:24 +0100

    2020.09

--- 2020/Solutions/Day09.cs
+++ 2020/Solutions/Day09.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using AdventOfCode2020.Solutions.Shared;
-using NUnit.Framework;
 
 namespace AdventOfCode2020.Solutions
 {
@@ -8,38 +9,57 @@ namespace AdventOfCode2020.Solutions
     {
         public void Solve()
         {
-            var input = Input.Text(nameof(Day09));
-            Console.WriteLine(this.Puzzle1(input));
-            Console.WriteLine(this.Puzzle2(input));
+            var input = Input.Lines(nameof(Day09)).Select(x => long.Parse(x)).ToArray();
+            var p1 = this.Puzzle1(input);
+            Console.WriteLine(p1);
+            Console.WriteLine(this.Puzzle2(input, p1));
         }
 
-        private int Puzzle1(string input)
+        private long Puzzle1(long[] input)
         {
-            return -1;
-        }
-
-        private int Puzzle2(string input)
-        {
-            return -1;
-        }
-
-        private class Tests
-        {
-            [Test]
-            public void Puzzle1()
+            for (var i = 25; i < input.Length; i++)
             {
-                var actual = new Day09().Puzzle1(TestInput);
-                Assert.AreEqual(-1, actual);
+                var combinations = GetLastCombinations(i - 25, input);
+                if (!combinations.Contains(input[i]))
+                    return input[i];
             }
 
-            [Test]
-            public void Puzzle2()
+            throw new InvalidProgramException();
+        }
+
+        private long Puzzle2(long[] input, long invalid)
+        {
+            for (var i = 0; i < input.Length; i++)
             {
-                var actual = new Day09().Puzzle2(TestInput);
-                Assert.AreEqual(-1, actual);
+                var current = new List<long> { input[i] };
+                var sum = input[i];
+                for (var j = i + 1; j < input.Length; j++)
+                {
+                    sum += input[j];
+                    current.Add(input[j]);
+                    if (sum == invalid)
+                    {
+                        return current.Min() + current.Max();
+                    }
+                }
             }
 
-            private readonly string TestInput = @"";
+            throw new InvalidProgramException();
+        }
+
+        private static List<long> GetLastCombinations(int start, long[] input)
+        {
+            var combinations = new List<long>();
+
+            for (var x = start; x < start + 25; x++)
+            {
+                for (var y = x + 1; y < start + 25; y++)
+                {
+                    combinations.Add(input[x] + input[y]);
+                }
+            }
+
+            return combinations;
         }
     }
 }
